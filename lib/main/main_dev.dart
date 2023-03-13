bdd9291902cdc022adce5aa6b8ddb506196a6fad
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-03-13 21:00:12 +0600

    config added to main

--- lib/main/main_dev.dart
+++ lib/main/main_dev.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:wozle/src/app.dart';
+import 'package:wozle/src/core/config/build_config.dart';
+import 'package:wozle/src/core/config/env_config.dart';
+import 'package:wozle/src/core/config/environment.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final envConfig = EnvConfig(
+    appName: packageInfo.appName,
+  );
+
+  BuildConfig.instantiate(
+    config: envConfig,
+    type: Environment.DEVELOPMENT,
+  );
+
   runApp(
     const Application(),
   );
--- lib/main/main_stage.dart
+++ lib/main/main_stage.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:wozle/src/app.dart';
+import 'package:wozle/src/core/config/build_config.dart';
+import 'package:wozle/src/core/config/env_config.dart';
+import 'package:wozle/src/core/config/environment.dart';
 
-void main() {
-  runApp(const Application());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final envConfig = EnvConfig(
+    appName: packageInfo.appName,
+  );
+
+  BuildConfig.instantiate(
+    config: envConfig,
+    type: Environment.DEVELOPMENT,
+  );
+
+  runApp(const Application(),);
 }
