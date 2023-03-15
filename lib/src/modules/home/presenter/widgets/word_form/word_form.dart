b51669657f843e981acd0c8979176d022096d754
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-03-15 20:50:35 +0600

    isActive parameter added

--- lib/src/modules/home/presenter/widgets/word_form/word_form.dart
+++ lib/src/modules/home/presenter/widgets/word_form/word_form.dart
@@ -4,10 +4,12 @@ import 'package:wozle/src/modules/home/presenter/widgets/character_field/charact
 
 class WordForm extends StatefulWidget {
   final int size;
+  final bool isActive;
 
   const WordForm({
     Key? key,
     required this.size,
+    required this.isActive,
   }) : super(key: key);
 
   @override
@@ -16,8 +18,9 @@ class WordForm extends StatefulWidget {
 
 class _WordFormState extends State<WordForm> with TickerProviderStateMixin {
   final List<AnimationController> _animationController = [];
-  AnimationStatus _status = AnimationStatus.dismissed;
   late List<Widget> _children;
+
+  // final WordFormController _controller = WordFormController();
 
   @override
   void initState() {
@@ -26,55 +29,13 @@ class _WordFormState extends State<WordForm> with TickerProviderStateMixin {
     _children = _buildChildren();
   }
 
-  List<Widget> _buildChildren() {
-    final List<Widget> children = [];
-
-    for (var index = 0; index < widget.size; index++) {
-      final animationController = AnimationController(
-        vsync: this,
-        duration: Duration(
-          milliseconds: (1000 * (index + 1)),
-        ),
-      );
-
-      animationController.addStatusListener((status) {
-        _status = status;
-        setState(() {});
-      });
-
-      _animationController.add(animationController);
-
-      if (index < widget.size - 1) {
-        children.add(
-          CharacterField(
-            index: index,
-            onChanged: (BuildContext context) {
-              FocusScope.of(context).nextFocus();
-            },
-            animation: _animationController[index],
-          ),
-        );
-      } else {
-        children.add(
-          CharacterField(
-            index: index,
-            onChanged: (BuildContext context) async {
-              FocusScope.of(context).unfocus();
-              await _startAnimation();
-            },
-            animation: _animationController[index],
-          ),
-        );
-      }
+  @override
+  void dispose() {
+    for (var element in _animationController) {
+      element.dispose();
     }
 
-    return children;
-  }
-
-  Future<void> _startAnimation() async {
-    for (var i = 0; i < widget.size; i++) {
-      _animationController[i].forward();
-    }
+    super.dispose();
   }
 
   @override
@@ -85,5 +46,51 @@ class _WordFormState extends State<WordForm> with TickerProviderStateMixin {
         children: _children,
       ),
     );
+  }
+
+  List<Widget> _buildChildren() {
+    final List<Widget> children = [];
+
+    for (var index = 0; index < widget.size; index++) {
+      _animationController.add(_getAnimationController(index));
+
+      children.add(
+        CharacterField(
+          index: index,
+          onChanged: (index < widget.size - 1)
+              ? (BuildContext context) {
+                  FocusScope.of(context).nextFocus();
+                }
+              : (BuildContext context) async {
+                  FocusScope.of(context).unfocus();
+                  await _startAnimation();
+                },
+          animation: _animationController[index],
+        ),
+      );
+    }
+
+    return children;
+  }
+
+  AnimationController _getAnimationController(int index) {
+    final animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: (800 * (index + 1)),
+      ),
+    );
+
+    animationController.addStatusListener((_) {
+      setState(() {});
+    });
+
+    return animationController;
+  }
+
+  Future<void> _startAnimation() async {
+    for (var i = 0; i < widget.size; i++) {
+      _animationController[i].forward();
+    }
   }
 }
