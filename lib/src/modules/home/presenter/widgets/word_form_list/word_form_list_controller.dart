aec56294da4d9f357aaf45f6cba56e24eaca28cd
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-03-17 07:43:45 +0600

    wordlist added for the previous solved puzzle

--- lib/src/modules/home/presenter/widgets/word_form_list/word_form_list_controller.dart
+++ lib/src/modules/home/presenter/widgets/word_form_list/word_form_list_controller.dart
@@ -1,7 +1,14 @@
 import 'package:get/get.dart';
 
 class WordFormListController extends GetxController {
+  final wordList = <String>[].obs;
   final activeIndex = 0.obs;
 
-  void incrementIndex() => activeIndex.value++;
+  void incrementIndex() {
+    activeIndex.value++;
+  }
+
+  void addWord(String word) {
+    wordList.add(word);
+  }
 }
