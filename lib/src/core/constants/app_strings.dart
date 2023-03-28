f2fa20c5dfaceb2f0e8f373af628fb1691b81aa6
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-03-28 10:49:30 +0600

    added navigation drawer string

--- lib/src/core/constants/app_strings.dart
+++ lib/src/core/constants/app_strings.dart
@@ -1,1 +1,7 @@
 const kAppName = "Wozle";
+
+const kHomeItemString1 = "Wozle";
+
+const kNavMenuItemString1 = "Settings";
+const kNavMenuItemString2 = "Statistics";
+const kNavMenuItemString3 = "About";
