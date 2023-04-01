d64255660880519a01ce6423b1be1a567085b2aa
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-01 11:59:27 +0600

    removed temp import

--- lib/src/modules/home/presenter/pages/home_screen.dart
+++ lib/src/modules/home/presenter/pages/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:wozle/src/modules/home/presenter/pages/home_screen_controller.dart';
 
 import 'package:wozle/src/modules/shared/app_bar/app_bar.dart';
-import 'package:wozle/src/modules/temp.dart';
 import 'package:wozle/src/modules/wozle/wozle.dart';
 import 'package:wozle/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer.dart';
 
