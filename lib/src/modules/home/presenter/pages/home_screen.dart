d30cf5c3c2dbe55e032a417b5b1df54ef997899d
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-01 06:42:47 +0600

    changed the app bar class name

--- lib/src/modules/home/presenter/pages/home_screen.dart
+++ lib/src/modules/home/presenter/pages/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:wozle/src/modules/home/presenter/widgets/word_form_list/word_form_list.dart';
 import 'package:wozle/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer.dart';
-import 'package:wozle/src/modules/shared/app_bar/wozle_app_bar.dart';
+import 'package:wozle/src/modules/shared/app_bar/app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const WozleAppBar(),
+      appBar: const ApplicationBar(),
       body: Container(
         padding: const EdgeInsets.all(8.0),
         margin: const EdgeInsets.all(8.0),
