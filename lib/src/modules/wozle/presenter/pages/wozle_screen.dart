9bee7a321b351e5f6c27a34c0eca5e4866ce1305
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-02 07:03:17 +0600

    changed the background with the theme

--- lib/src/modules/wozle/presenter/pages/wozle_screen.dart
+++ lib/src/modules/wozle/presenter/pages/wozle_screen.dart
@@ -10,7 +10,7 @@ class WozleScreen extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      color: Theme.of(context).colorScheme.primary,
+      color: Theme.of(context).colorScheme.background,
       child: const WordFormList(),
     );
   }
