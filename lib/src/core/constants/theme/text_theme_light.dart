45c08e6f29be1511a37bc805188c5ceaa4a3d137
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-03 08:33:49 +0600

    added text theme

---
+++ lib/src/core/constants/theme/text_theme_light.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+const textThemeLight = TextTheme(
+  displayLarge: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 57,
+    fontWeight: FontWeight.w400,
+  ),
+  displayMedium: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 45,
+    fontWeight: FontWeight.w400,
+  ),
+  displaySmall: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 36,
+    fontWeight: FontWeight.w400,
+  ),
+  headlineLarge: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+  ),
+  headlineMedium: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+  ),
+  headlineSmall: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  ),
+  titleLarge: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.0,
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  ),
+  titleMedium: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.15,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  ),
+  titleSmall: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.1,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  ),
+  labelLarge: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.1,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  ),
+  labelMedium: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.5,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  ),
+  labelSmall: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.5,
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+  ),
+  bodyLarge: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.15,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  ),
+  bodyMedium: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.25,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  ),
+  bodySmall: TextStyle(
+    color: Colors.black,
+    letterSpacing: 0.4,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  ),
+);
