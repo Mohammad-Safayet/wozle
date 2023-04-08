1ad0ded99a75cf7a2076ef5f5f3825aca081177b
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-08 14:29:11 +0600

    updated the home view

--- lib/src/modules/home/presenter/pages/home_screen.dart
+++ lib/src/modules/home/presenter/pages/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:wozle/src/modules/home/presenter/pages/home_screen_controller.dart';
-
+import 'package:wozle/src/core/routes/app_pages.dart';
 import 'package:wozle/src/modules/shared/app_bar/app_bar.dart';
-import 'package:wozle/src/modules/wozle/wozle.dart';
 import 'package:wozle/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer.dart';
+
+import 'package:wozle/src/modules/shared/dialogs/info_dialog.dart';
+import 'package:wozle/src/modules/wozle/wozle.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -12,18 +13,36 @@ class HomeScreen extends StatelessWidget {
   final homeScreenList = [
     const WozleView(),
   ];
-  final _controller = Get.find<HomeScreenContoller>();
 
   @override
   Widget build(BuildContext context) {
+    bool isTrue = false;
+    if (isTrue) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Add Your Code here.
+    Get.toNamed(Routes.WOZLE);
+    });
+    } else {
+      Future.delayed(
+        Duration.zero,
+        () => showDialog(
+          barrierDismissible: false,
+          context: context,
+          builder: (context) => InfoDialog(
+            onClose: () {
+              Get.toNamed(Routes.WOZLE);
+            },
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: const ApplicationBar(),
-      body: Obx(
-        () => homeScreenList[_controller.currentIndex.value],
+      body: Container(
+        color: Theme.of(context).colorScheme.background,
       ),
-      drawer: NavDrawer(
-        contoller: _controller,
-      ),
+      drawer: NavDrawer(),
     );
   }
 }
