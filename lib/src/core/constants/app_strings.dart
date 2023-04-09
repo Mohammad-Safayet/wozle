f693b46cceda2ebe6f45bfe98ada606999e36d52
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-09 17:45:25 +0600

    add shared_prefrences key

--- lib/src/core/constants/app_strings.dart
+++ lib/src/core/constants/app_strings.dart
@@ -14,3 +14,5 @@ const kPlayInstruction2 = "The color of tiles indicates how close you were";
 const kPlayExample1 = "The letter(s) in the green space is in the word and in the correct spot";
 const kPlayExample2 = "The letter(s) in the yellow space is in the word but not in the correct spot";
 const kPlayExample3 = "The letter(s) in the grey space is not in the word";
+
+const kSpOnStartKey = "latif.safayet.wozle-onStartKey";
