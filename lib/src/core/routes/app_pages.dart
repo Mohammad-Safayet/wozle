fdf2dc71a844efc683c765755884f23475b8ed0e
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-14 07:53:21 +0600

    removed the binding

--- lib/src/core/routes/app_pages.dart
+++ lib/src/core/routes/app_pages.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 
 import 'package:wozle/src/modules/home/home.dart';
 import 'package:wozle/src/modules/settings/settings.dart';
-import 'package:wozle/src/modules/shared/app_bar/presenter/bindings/nav_drawer_binding.dart';
 import 'package:wozle/src/modules/wozle/presenter/bindings/word_form_bindings.dart';
 import 'package:wozle/src/modules/wozle/presenter/bindings/word_form_list_binding.dart';
 import 'package:wozle/src/modules/wozle/wozle.dart';
@@ -18,9 +17,6 @@ class AppPages {
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
-      bindings: [
-        NavBarBinding(),
-      ],
     ),
     GetPage(
       name: _Paths.WOZLE,
