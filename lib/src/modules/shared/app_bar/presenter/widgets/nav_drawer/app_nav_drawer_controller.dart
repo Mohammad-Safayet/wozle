9716272095062fb6114608f61aa9a1a5056affb0
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-14 07:52:45 +0600

    changed navigation controller

--- lib/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class NavBarController extends GetxController {
-  final route = Rxn<String>(Get.currentRoute);
-
-  void changeIndex(String value) {
-    route.value = value;
-  }
-}
