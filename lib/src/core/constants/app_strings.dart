00f1a85362e0a10d044a552b7b6f0808d1e2e126
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-17 07:49:12 +0600

    added SharedPreferences key

--- lib/src/core/constants/app_strings.dart
+++ lib/src/core/constants/app_strings.dart
@@ -2,11 +2,13 @@ const kAppName = "Wozle";
 
 const kHomeItemString1 = "Wozle";
 
+// Navigation Item Strings
 const kNavMenuItemString1 = "Settings";
 const kNavMenuItemString2 = "Statistics";
 const kNavMenuItemString3 = "How to play";
 const kNavMenuItemString4 = "About";
 
+// How to Play Strings
 const kPlayInstructionHeading = "How To Play";
 const kPlayInstructionTitle = "Guess the word in 6 tries";
 const kPlayInstruction1 = "Each guess must be a valid 5-letter word";
@@ -15,4 +17,7 @@ const kPlayExample1 = "The letter(s) in the green space is in the word and in th
 const kPlayExample2 = "The letter(s) in the yellow space is in the word but not in the correct spot";
 const kPlayExample3 = "The letter(s) in the grey space is not in the word";
 
+// SharedPreferences Key Strings
 const kSpOnStartKey = "latif.safayet.wozle-onStartKey";
+const kSpIsDarkMode = "latif.safayet.wozle-IsDarkMode";
+const kSpIsHardMode = "latif.safayet.wozle-IsHardMode";
