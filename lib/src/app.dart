ac620d9b4acb08010caa85752714f8c7494ca019
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-04-26 21:26:18 +0600

    added thememode to app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
 import 'package:wozle/src/core/constants/theme/color_scheme_dark.dart';
 import 'package:wozle/src/core/constants/theme/color_scheme_light.dart';
 import 'package:wozle/src/core/constants/theme/text_theme_dark.dart';
 import 'package:wozle/src/core/constants/theme/text_theme_light.dart';
 import 'package:wozle/src/core/routes/app_pages.dart';
+import 'package:wozle/src/modules/shared/drivers/settings_service.dart';
 
-class Application extends StatelessWidget {
-  const Application({
+class Application extends StatefulWidget {
+ const Application({
     Key? key,
     required this.applicationName,
   }) : super(key: key);
 
   final String applicationName;
 
+  // ignore: library_private_types_in_public_api
+  static _ApplicationState of(BuildContext context) =>
+      context.findAncestorStateOfType<_ApplicationState>()!;
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  final settings = SettingsService.to;
+
+  late ThemeMode _themeMode;
+  String initialPage = AppPages.INITIAL;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final isDarkModeOn = settings.isDarkModeOn.value;
+    (isDarkModeOn) ? _themeMode = ThemeMode.dark : _themeMode = ThemeMode.light;
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return GetMaterialApp(
-      title: applicationName,
-      initialRoute: AppPages.INITIAL,
+      title: widget.applicationName,
+      initialRoute: initialPage,
       getPages: AppPages.routes,
       theme: Theme.of(context).copyWith(
         useMaterial3: true,
@@ -31,6 +56,16 @@ class Application extends StatelessWidget {
         colorScheme: appColorSchemeDark,
         textTheme: textThemeDark,
       ),
+      themeMode: _themeMode,
     );
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    Logger().d("changeTheme triggered! $_themeMode $themeMode");
+    setState(() {
+      _themeMode = themeMode;
+      initialPage = Routes.SETTINGS;
+    });
+    Logger().d("changeTheme triggered! $_themeMode $themeMode");
   }
 }
