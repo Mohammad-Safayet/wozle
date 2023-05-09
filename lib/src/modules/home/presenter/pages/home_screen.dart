462ac07d694cf6ce5ea7f82a173f5bb28ca9dbd7
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-05-09 07:55:53 +0600

    added homepage import updated

--- lib/src/modules/home/presenter/pages/home_screen.dart
+++ lib/src/modules/home/presenter/pages/home_screen.dart
@@ -8,14 +8,14 @@ import 'package:wozle/src/modules/home/presenter/pages/home_screen_controller.da
 import 'package:wozle/src/modules/shared/app_bar/app_bar.dart';
 import 'package:wozle/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer.dart';
 import 'package:wozle/src/modules/shared/dialogs/info_dialog.dart';
-import 'package:wozle/src/modules/shared/drivers/storage_service.dart';
+import 'package:wozle/src/modules/shared/drivers/shared_prefs_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final storageService = StorageService.to;
+    final storageService = SharedPrefsService.to;
     final isStartDialogOff = storageService.getBool(kSpOnStartKey) ?? false;
     final controller = HomeScreenController.to;
 
