4d21fbf6b4a14c6c483b725e8345c4eec65c2053
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-06-08 07:54:53 +0600

    chaned the method signature

--- lib/src/modules/wozle/infra/datasources/word_datasource.dart
+++ lib/src/modules/wozle/infra/datasources/word_datasource.dart
@@ -1,18 +1,7 @@
-import 'dart:convert';
+import 'package:wozle/src/modules/wozle/infra/models/daily_word.dart';
 
-import 'package:wozle/src/modules/wozle/infra/models/word.dart';
+abstract class WordDataSource {
+  Future<DailyWord?> getData();
 
-abstract class WordRemoteDataSource {
-  Future<Word> getData(
-    String endPoint, {
-    Map<String, String>? queryParams,
-    Map<String, String>? headers,
-  });
-
-  Future<dynamic> postData(
-    String endPoint, {
-    Map<String, String>? headers,
-    Object? body,
-    Encoding? encoding,
-  });
+  Future<dynamic> saveData(DailyWord dailyWord);
 }
