051af4fd9e06678932e0e9dcc250554aa14c3c2e
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-06-18 08:05:46 +0600

    added unknown error failure

--- lib/src/core/extensions/error_object_extension.dart
+++ lib/src/core/extensions/error_object_extension.dart
@@ -5,17 +5,17 @@ extension ErrorObjectExtension on ErrorObject {
   static ErrorObject mapToErrorObject({required FailureEntity failure}) {
     return failure.when(
       serverFailure: () => const ErrorObject(
-        title: 'Error Code: INTERNAL_SERVER_FAILURE',
+        title: 'INTERNAL SERVER FAILURE',
         message:
             'It seems that the server is not reachable at the moment, try again later, should the issue persist please reach out to the developer.',
       ),
       noConnectionFailure: () => const ErrorObject(
-        title: 'Error Code: NO_CONNECTIVITY',
+        title: 'NO CONNECTIVITY',
         message:
             'It seems that your device is not connected to the network, please check your internet connectivity or try again later.',
       ),
       unknownFailure: () => const ErrorObject(
-        title: 'Error Code: UNKNOWN',
+        title: 'UNKNOWN ERROR OCCOURED',
         message:
             'It seems that some unexpected error occoured. Please try again later.',
       ),
