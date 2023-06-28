09ec6e28752f66f62e54432258f1c2f0f7a7dd78
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-06-28 08:13:47 +0600

    added registration of the ProgressEntityAdapter

--- lib/src/modules/shared/services/hive_service.dart
+++ lib/src/modules/shared/services/hive_service.dart
@@ -1,6 +1,7 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:wozle/src/modules/wozle/domain/entities/entities.dart';
+import 'package:wozle/src/modules/wozle/domain/entities/progression_entity.dart';
 
 class HiveService {
   static Future init() async {
@@ -9,6 +10,6 @@ class HiveService {
     Hive.registerAdapter(WordEntityAdapter());
     Hive.registerAdapter(MeaningEntityAdapter());
     Hive.registerAdapter(DefinitionEntityAdapter());
-
+    Hive.registerAdapter(ProgressionEntityAdapter());
   }
 }
