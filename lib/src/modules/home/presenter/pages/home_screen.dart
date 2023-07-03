8d4efc1049750dac51195dac74ab1de32609eeaf
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-03 08:44:45 +0600

    added home navigation list items

--- lib/src/modules/home/presenter/pages/home_screen.dart
+++ lib/src/modules/home/presenter/pages/home_screen.dart
@@ -6,7 +6,7 @@ import 'package:wozle/src/modules/home/presenter/pages/home_screen_controller.da
 
 import 'package:wozle/src/modules/shared/app_bar/app_bar.dart';
 import 'package:wozle/src/modules/shared/app_bar/presenter/widgets/nav_drawer/app_nav_drawer.dart';
-import 'package:wozle/src/modules/shared/dialogs/info_dialog.dart';
+import 'package:wozle/src/modules/wozle/presenter/dialogs/info_dialog.dart';
 import 'package:wozle/src/modules/shared/services/shared_prefs_service.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -45,7 +45,9 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         color: Theme.of(context).colorScheme.background,
       ),
-      drawer: NavDrawer(),
+      drawer: NavDrawer(
+        features: const [],
+      ),
     );
   }
 }
