78309a3e2f3f821de99b6ad48212f0e51cf1ac47
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-19 08:45:22 +0600

    updated the initialization and close method

--- lib/src/core/drivers/local_storage/local_storage_impl.dart
+++ lib/src/core/drivers/local_storage/local_storage_impl.dart
@@ -5,12 +5,20 @@ import 'package:wozle/src/core/drivers/local_storage/local_storage.dart';
 class LocalStorageImpl extends LocalStorage {
   @override
   Future<void> closeBox() async {
-    await Hive.close();
+    try {
+      await Hive.close();
+    } catch (exception) {
+      rethrow;
+    }
   }
 
   @override
   Future<void> initialize() async {
-    await Hive.initFlutter();
+    try {
+      await Hive.initFlutter();
+    } catch (exception) {
+      rethrow;
+    }
   }
 
   @override
@@ -19,5 +27,4 @@ class LocalStorageImpl extends LocalStorage {
 
     return box;
   }
-  
 }
