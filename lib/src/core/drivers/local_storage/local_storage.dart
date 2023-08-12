982fae2a50acec7226775c4c16d5cdd2628d6d88
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-12 06:28:36 +0600

    added adapters on initialize

--- lib/src/core/drivers/local_storage/local_storage.dart
+++ lib/src/core/drivers/local_storage/local_storage.dart
@@ -1,7 +1,9 @@
 import 'package:hive/hive.dart';
 
 abstract class LocalStorage {
-  Future<void> initialize();
+  Future<void> initialize({
+    List<TypeAdapter<dynamic>> adapters,
+  });
 
   Future<Box> openBox(String name);
 
