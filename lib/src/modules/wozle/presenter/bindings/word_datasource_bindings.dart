9a013fc052e237131a4ce32a1c07c76ccb0bfcbb
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-12 11:21:45 +0600

    added localstorage parameter

--- lib/src/modules/wozle/presenter/bindings/word_datasource_bindings.dart
+++ lib/src/modules/wozle/presenter/bindings/word_datasource_bindings.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart';
 
 import 'package:wozle/src/core/constants/app_strings.dart';
 import 'package:wozle/src/core/drivers/http/http_driver_impl.dart';
-import 'package:wozle/src/core/drivers/local_storage/local_storage_impl.dart';
 import 'package:wozle/src/modules/shared/services/local_storage_service.dart';
 import 'package:wozle/src/modules/wozle/infra/datasources/word_datasource/local/word_local_datasource_impl.dart';
 import 'package:wozle/src/modules/wozle/infra/datasources/word_datasource/remote/word_remote_datasource_impl.dart';
