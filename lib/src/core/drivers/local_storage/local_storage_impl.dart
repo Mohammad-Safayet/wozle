6591e009ccea7e454f1ab8369b8d397d35824c36
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-13 08:59:33 +0600

    added register adapter manually

--- lib/src/core/drivers/local_storage/local_storage_impl.dart
+++ lib/src/core/drivers/local_storage/local_storage_impl.dart
@@ -1,5 +1,6 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'package:wozle/src/modules/wozle/domain/entities/entities.dart';
 import 'package:wozle/src/core/drivers/local_storage/local_storage.dart';
 
 class LocalStorageImpl extends LocalStorage {
@@ -18,11 +19,16 @@ class LocalStorageImpl extends LocalStorage {
   }) async {
     try {
       await Hive.initFlutter();
-      if (adapters.isNotEmpty) {
-        for (var adapter in adapters) {
-          Hive.registerAdapter(adapter);
-        }
-      }
+      // if (adapters.isNotEmpty) {
+      //   for (var adapter in adapters) {
+      //     Hive.registerAdapter(adapter);
+      //   }
+      // }
+      Hive.registerAdapter(DailyWordEntityAdapter());
+      Hive.registerAdapter(WordEntityAdapter());
+      Hive.registerAdapter(MeaningEntityAdapter());
+      Hive.registerAdapter(DefinitionEntityAdapter());
+      Hive.registerAdapter(ProgressionEntityAdapter());
     } catch (exception) {
       rethrow;
     }
