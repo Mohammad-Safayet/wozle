acbd90720e8159e9ee10a0730098479afb51c0df
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-16 07:12:45 +0600

    added get_daily_word_usecase_test

---
+++ test/src/modules/wozle/domain/usecases/get_daily_word_usecase_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+void main() {
+  group("Get Daily Word Usecase Test", () {
+    setUpAll(() {});
+
+    tearDownAll(() {
+      resetMockitoState();
+    });
+  });
+}
