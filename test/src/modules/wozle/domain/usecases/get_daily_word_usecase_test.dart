cd9cb7491717c7db62ad74e67aea77574073b664
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-16 07:18:34 +0600

    usecase test: added WordRepository mock

--- test/src/modules/wozle/domain/usecases/get_daily_word_usecase_test.dart
+++ test/src/modules/wozle/domain/usecases/get_daily_word_usecase_test.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:wozle/src/modules/wozle/infra/repositories/word_repo.dart';
 
+@GenerateMocks([
+  WordRepository
+],)
 void main() {
   group("Get Daily Word Usecase Test", () {
     setUpAll(() {});
