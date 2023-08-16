8d3d64a5a81916d6d99e369e1a6f8ba302acb258
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-16 07:58:36 +0600

    usecase test: bug fixed

--- test/src/modules/wozle/domain/usecases/get_daily_word_usecase_test.dart
+++ test/src/modules/wozle/domain/usecases/get_daily_word_usecase_test.dart
@@ -7,6 +7,7 @@ import 'package:mockito/mockito.dart';
 import 'package:wozle/src/core/utils/error_handling/error_handling.dart';
 import 'package:wozle/src/core/utils/error_handling/execptions.dart';
 import 'package:wozle/src/modules/wozle/domain/usecases/get_daily_word.dart';
+import 'package:wozle/src/modules/wozle/infra/models/daily_word.dart';
 import 'package:wozle/src/modules/wozle/infra/repositories/word_repo.dart';
 
 import 'get_daily_word_usecase_test.mocks.dart';
@@ -14,6 +15,7 @@ import 'get_daily_word_usecase_test.mocks.dart';
 @GenerateMocks(
   [
     WordRepository,
+    DailyWord,
   ],
 )
 void main() {
@@ -31,12 +33,12 @@ void main() {
     test(
       "return ServerFailure on ServerException",
       () async {
-        when(getDailyWordUsecase.call()).thenThrow(
+        when(mockWordRepository.getDailyWord()).thenThrow(
           ServerException(),
         );
 
         final data = await getDailyWordUsecase.call();
-        
+
         expect(
           data,
           const Left(
@@ -45,16 +47,16 @@ void main() {
         );
       },
     );
-    
+
     test(
       "return NoConnectionFailure on NoConnectionException",
       () async {
-        when(getDailyWordUsecase.call()).thenThrow(
+        when(mockWordRepository.getDailyWord()).thenThrow(
           NoConnectionException(),
         );
 
         final data = await getDailyWordUsecase.call();
-        
+
         expect(
           data,
           const Left(
@@ -63,16 +65,16 @@ void main() {
         );
       },
     );
-    
+
     test(
       "return NoConnectionFailure on TimeoutException",
       () async {
-        when(getDailyWordUsecase.call()).thenThrow(
+        when(mockWordRepository.getDailyWord()).thenThrow(
           TimeoutException(""),
         );
 
         final data = await getDailyWordUsecase.call();
-        
+
         expect(
           data,
           const Left(
